74f6976dc917c89d79d164910e42ee46b6bf1a01
Florian Jud <florian@famjud.de>
2017-03-28 10:46:57 +0200

    Several Optimazations

--- Art Book/Art Book/ViewController.swift
+++ Art Book/Art Book/ViewController.swift
@@ -7,35 +7,102 @@
 //
 
 import UIKit
-
+import CoreData
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
 
     @IBOutlet weak var tableView: UITableView!
     
+    var nameArray = [String]()
+    var yearArray = [Int]()
+    var artistArray = [String]()
+    var imageArray = [UIImage]()
+    var selectedPainting = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
         
         //setup table view
         tableView.dataSource = self
         tableView.delegate = self
+        
+        retrieveInfo()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.retrieveInfo), name: NSNotification.Name("paintingCreated"), object: nil)
+    }
+    
+    //Hole die Daten aus der Datenbank
+    func retrieveInfo(){
+        
+        self.nameArray.removeAll(keepingCapacity: false)
+        self.yearArray.removeAll(keepingCapacity: false)
+        self.artistArray.removeAll(keepingCapacity: false)
+        self.imageArray.removeAll(keepingCapacity: false)
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Paintings")
+        fetchRequest.returnsObjectsAsFaults = false
+        
+        do{
+            let results = try context.fetch(fetchRequest)
+            if results.count > 0{
+                for result in results as! [NSManagedObject]{
+                    if let name = result.value(forKey: "name") as? String{
+                        self.nameArray.append(name)
+                    }
+                    if let year = result.value(forKey: "year") as? Int{
+                        self.yearArray.append(year)
+                    }
+                    if let artist = result.value(forKey: "artist") as? String{
+                        self.artistArray.append(artist)
+                    }
+                    if let imageData = result.value(forKey: "image") as? Data{
+                        let image = UIImage(data: imageData)
+                        self.imageArray.append(image!)
+                    }
+                    
+                    self.tableView.reloadData()
+                }
+            }
+        }catch{
+            print("There was an error")
+            
+        }
     }
 
 
     @IBAction func addButtonClicked(_ sender: Any) {
+        self.selectedPainting = ""
         performSegue(withIdentifier: "toCreateVC", sender: nil)
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return nameArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        cell.textLabel?.text = "I've set table view"
+        cell.textLabel?.text = nameArray[indexPath.row]
         return cell
     }
 
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.selectedPainting = nameArray[indexPath.row]
+        performSegue(withIdentifier: "toCreateVC", sender: nil)
+    }
+    
+    //Prepare Segue
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toCreateVC"{
+            let destinationVC =  segue.destination as! createVC
+            destinationVC.chosenPainting =  selectedPainting
+        }
+    }
     
 }
 
--- Art Book/Art Book/createVC.swift
+++ Art Book/Art Book/createVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 class createVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
 
@@ -15,14 +16,58 @@ class createVC: UIViewController, UIImagePickerControllerDelegate, UINavigationC
     @IBOutlet weak var artistText: UITextField!
     @IBOutlet weak var yearText: UITextField!
     
+    var chosenPainting = ""
+    
     //Bei Klick auf das Bild soll ein Foto hochgeladen werden können
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        if chosenPainting != "" {
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            let context = appDelegate.persistentContainer.viewContext
+            
+            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Paintings")
+            fetchRequest.predicate = NSPredicate(format: "name = %@", self.chosenPainting)
+            fetchRequest.returnsObjectsAsFaults = false
+            
+            do{
+                let results = try context.fetch(fetchRequest)
+                if results.count > 0{
+                    for result in results as! [NSManagedObject]{
+                        if let name = result.value(forKey: "name") as? String{
+                            nameText.text = name
+                        }
+                        
+                        if let year = result.value(forKey: "year") as? Int{
+                            yearText.text = String(year)
+                        }
+                        
+                        if let artist = result.value(forKey: "artist") as? String{
+                            artistText.text = artist
+                        }
+                        if let imageData = result.value(forKey: "image") as? Data{
+                            let image = UIImage(data: imageData)
+                            self.imageView.image = image
+                        }
+                    }
+                }
+                
+            }catch{
+                print("An error occoured")
+            }
+        }else{
+            imageView.image = UIImage(named: "tapme.png")
+            nameText.text = ""
+            yearText.text = ""
+            artistText.text = ""
+        }
+        
+        
         //Bild Gestensteuereung Aktivierung
         imageView.isUserInteractionEnabled = true
         let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(createVC.selectImage))
         imageView.addGestureRecognizer(gestureRecognizer)
+        
         
     }
     
@@ -37,11 +82,39 @@ class createVC: UIViewController, UIImagePickerControllerDelegate, UINavigationC
 
     //Was soll passieren, wenn ein Photo ausgewählt wurde
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
         imageView.image = info[UIImagePickerControllerEditedImage] as? UIImage
+        
         self.dismiss(animated: true, completion: nil)
     }
     
     @IBAction func saveButtonClicked(_ sender: Any) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let newArt = NSEntityDescription.insertNewObject(forEntityName: "Paintings", into: context)
+        newArt.setValue(nameText.text, forKey: "name")
+        newArt.setValue(artistText.text, forKey: "artist")
+        if let year = Int(yearText.text!) {
+          newArt.setValue(year, forKey: "year")
+        }
+        else{
+            newArt.setValue(0, forKey: "year")
+        }
+        
+        let data = UIImageJPEGRepresentation(imageView.image!, 0.5)
+        newArt.setValue(data, forKey: "image")
+        
+        do{
+            try context.save()
+            print("we managed  to save it successfully")
+        }catch{
+            print("error")
+        }
+        
+        
+        NotificationCenter.default.post(name: NSNotification.Name(rawValue: "paintingCreated"), object: nil)
+        _ = self.navigationController?.popViewController(animated: true)
     }
 
 }
